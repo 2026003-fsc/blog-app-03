24f25c45f3a379fbcdec4e0d870d7f56ae9df435
Furugo <2026003@fsc-educ.com>
2026-06-08 17:09:18 +0900

    6/8③

--- src/main/java/com/example/blog_app/BlogController.java
+++ src/main/java/com/example/blog_app/BlogController.java
@@ -1,13 +1,21 @@
 package com.example.blog_app;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class BlogController {
+
+    private final BlogService blogService;
+
+    BlogController(BlogService blogService) {
+        this.blogService = blogService;
+    }
 
     @GetMapping("/")
     public String home(){
@@ -19,6 +27,10 @@ public class BlogController {
         return "post";
     }
 
-    
+    @GetMapping("/blogs")
+    public String blogs(@RequestParam(required = false) String keyword, Model model){
+        model.addAttribute("blogs", blogService.search(keyword));
+        return "blogs";
+    }
 
 }
